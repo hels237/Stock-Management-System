aa29dd1690787506bb799e25fb37b42f73aa5166
hels <m7hels@gmail.com>
2024-03-13 17:05:07 +0100

    adding AbstractEntity class

---
+++ src/main/java/com/example/helscorp/stockmanagement/model/AbstractEntity.java
@@ -0,0 +1,50 @@
+package com.example.helscorp.stockmanagement.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Date;
+
+@MappedSuperclass
+public class AbstractEntity  implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @CreatedDate
+    @Column(name = "creationData",nullable = false)
+    @JsonIgnore
+    private Instant creationDate;
+    @LastModifiedDate
+    @Column(name = "lastModifiedUpdate")
+    @JsonIgnore
+    private Instant lastUpdate;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Instant getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Instant creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Instant getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(Instant lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+}
