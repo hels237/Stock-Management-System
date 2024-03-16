dd30c56d1d720edd8d92129dc3479175badb20ed
hels <m7hels@gmail.com>
2024-03-16 06:01:16 +0100

    add entities

--- src/main/java/com/example/helscorp/stockmanagement/model/AbstractEntity.java
+++ src/main/java/com/example/helscorp/stockmanagement/model/AbstractEntity.java
@@ -4,12 +4,14 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.io.Serializable;
 import java.time.Instant;
 import java.util.Date;
 
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)//he would listenimg the object and update the field accordently
 public class AbstractEntity  implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
---
+++ src/main/java/com/example/helscorp/stockmanagement/model/CmdeFournisseur.java
@@ -0,0 +1,4 @@
+package com.example.helscorp.stockmanagement.model;
+
+public class CmdeFournisseur {
+}
